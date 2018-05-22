f34a1b3d32e5d41481573aa1db13de102b11b5a2
Parakoopa <parakoopa@live.de>
2018-05-22 19:20:08 +0200

    Einrückungen in main.ts korrigiert

--- src/client/scripts/main.ts
+++ src/client/scripts/main.ts
@@ -3,14 +3,12 @@
 
     import {Game} from "./Game";
     import {RequestManager} from "./RequestManager";
-    import {bla} from "./test";
 
     export function main() {
 
-    const game: Game = new Game(); // Phaser
-    const reqManager = new RequestManager(game);
-    reqManager.EventListener();
-   // bla();
-    reqManager.JoinRoom("TestRoom");
+        const game: Game = new Game(); // Phaser
+        const reqManager = new RequestManager(game);
+        reqManager.EventListener();
+        reqManager.JoinRoom("TestRoom");
 
-}
+    }
