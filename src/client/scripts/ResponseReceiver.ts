18417c1d15dd56680828e911d0132605a413899f
Tim Beier <tim.beier@haw-hamburg.de>
2018-06-06 16:38:40 +0200

    [Refactoring] removed deprecated code

--- src/client/scripts/ResponseReceiver.ts
+++ src/client/scripts/ResponseReceiver.ts
@@ -53,29 +53,6 @@ export class ResponseReceiver {
         });
     }
 
-    public joinRoom(roomName: string): void {
-        const playerKey = this._playerKey;
-        this._socket.emit("joinRoom", {request: "joinRoom", playerKey, roomName});
-    }
-
-    public leaveRoom(): void {
-        const playerKey = this._playerKey;
-        const roomKey = this._roomKey;
-        this._socket.emit("leaveRoom", {request: "leaveRoom", playerKey, roomKey});
-    }
-
-    public startGame(sizeX: number, sizeY: number): void {
-        const playerKey = this._roomKey;
-        const roomKey = this._roomKey;
-        this._socket.emit("startGame", {request: "startGame", playerKey, roomKey, sizeX, sizeY});
-    }
-
-    public placeTile(x: number, y: number): void {
-        const playerKey = this._playerKey;
-        const roomKey = this._roomKey;
-        this._socket.emit("placeTile" , {request: "placeTile", playerKey, roomKey, x, y});
-    }
-
     /**
      *
      * @param {IRoomIsFullResponse | IJoinedResponse} resp
