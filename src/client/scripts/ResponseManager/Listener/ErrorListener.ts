7e157f467c6cdbb9150fbca4ca7c784e3dd9ef2e
Tim Beier <tim.beier@haw-hamburg.de>
2018-06-22 18:45:35 +0200

    [#53] cleanup

--- src/client/scripts/ResponseManager/Listener/ErrorListener.ts
+++ src/client/scripts/ResponseManager/Listener/ErrorListener.ts
@@ -5,32 +5,32 @@ export class ErrorListener {
 
     /**
      * Listen for ErrorResponses on socket to be displayed via UiManager
-     * @param {SocketIOClient.Socket} _socket
+     * @param {SocketIOClient.Socket} socket
      * @param {UiManager} _uiManager
      */
-    public static listen(_socket: Socket, _uiManager: UiManager) {
+    public static listen(socket: Socket, _uiManager: UiManager) {
 
         // Error Feedback
         // Actions
-        _socket.on("roomIsFull", (resp: IRoomIsFullResponse) => {
+        socket.on("roomIsFull", (resp: IRoomIsFullResponse) => {
             _uiManager.textElement(resp.response);
         });
-        _socket.on("refresh", (resp: IRefreshResponse) => {
+        socket.on("refresh", (resp: IRefreshResponse) => {
             _uiManager.textElement("PLEASE REFRESH PAGE");
         });
-        _socket.on("observer", () => {
+        socket.on("observer", () => {
             _uiManager.textElement("Observers to not play!");
         });
-        _socket.on("notYourTurn", (resp: INotYourTurnResponse) => {
+        socket.on("notYourTurn", (resp: INotYourTurnResponse) => {
             _uiManager.textElement("It is not your turn!");
         });
-        _socket.on("notInRoom", () => {
+        socket.on("notInRoom", () => {
             _uiManager.textElement("You are not in a room!");
         });
-        _socket.on("alreadyInRoom", () => {
+        socket.on("alreadyInRoom", () => {
             _uiManager.textElement("You are already in a room!");
         });
-        _socket.on("notOwner", () => {
+        socket.on("notOwner", () => {
             _uiManager.textElement("You are not the room owner!");
         });
     }
