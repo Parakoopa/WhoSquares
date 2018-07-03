6dd2707c020d5f872ae7d9a2dd96028f414db13f
Robin Müller <robin.mueller2@haw-hamburg.de>
2018-07-03 15:00:23 +0200

    header banner added

--- src/client/scripts/ui/Header.tsx
+++ src/client/scripts/ui/Header.tsx
@@ -13,7 +13,7 @@ export class Header extends React.Component<IHeaderProps, IHeaderState> {
     public render(): any {
 
         return <div className={"header"} id="header">
-            <a href="https://www.w3schools.com">
+            <a href="https://gitlab.informatik.haw-hamburg.de/wp-mbc-ss2018/who-squares">
             <img alt="Who Squares?" src="../../img/header.png" width="500em" height="140em" />
             </a>
         </div>;
