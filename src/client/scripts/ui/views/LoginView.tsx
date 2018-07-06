09883e806b3088ada98ba684bedad424f3621752
Alexander Matthiesen <alexander.matthiesen@haw-hamburg.de>
2018-07-06 12:10:52 +0200

    Updated Components  - RoomView doesn't contain Component "Room", but contains now the direct child components  - LobbyView doesn't contain Compnent "Lobby", but contains now the direct child components

--- src/client/scripts/ui/views/LoginView.tsx
+++ src/client/scripts/ui/views/LoginView.tsx
@@ -1,21 +1,66 @@
 import * as React from "react";
-import {Login} from "../components/Login";
+import {Connection} from "../../Connection";
+import {ILoginUI} from "../interfaces/ILoginUI";
+import {Routes} from "../Routes";
 
 export interface ILoginViewProps {
-
 }
+
 export interface ILoginViewState {
-
+    username: string;
 }
 
-export class LoginView extends React.Component<ILoginViewProps, ILoginViewState> {
-    public render() {
+export class LoginView extends React.Component<ILoginViewProps, ILoginViewState> implements ILoginUI {
 
-        return <div className={"view"}>
-            {/* Die Game(View) Komponente lädt aktuell einfach den aktuellen Spiel-Canvas. Später muss man der wohl
-                noch beibringen zbs. welcher Raum betreten werden soll? Das würde ich dann hier
-                als Prop mitgeben: */}
-            <Login/>
+    constructor(props: ILoginViewProps) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+        const username = Connection.getUsername();
+
+        this.state = {username};
+    }
+
+    private validateForm() {
+        if (this.state.username === undefined)
+            return false;
+        else
+            return this.state.username.length > 0;
+    }
+
+    private handleChange(event: any) {
+        this.setState({username: event.target.value});
+    }
+
+    private handleSubmit(event: any) {
+        event.preventDefault();
+
+        this.login();
+    }
+
+    private login(): void {
+        const ok = Connection.login(this.state.username, () => {
+            window.location.href = Routes.linkToLobbyHREF();
+        });
+
+        if (!ok)
+            alert("Connection failed, please try again!");
+    }
+
+    public render() {
+        return <div className={"content"}>
+            <form onSubmit={this.handleSubmit}>
+                <h3 className={"description"}>username:</h3>
+                <div className={"content"}>
+                    <span id={"empty"}/>
+                    <input className={"input"} type="text"
+                           value={this.state.username} onChange={this.handleChange}/>
+                    <input className={"button"} type="submit"
+                           disabled={!this.validateForm()} value="OK"/>
+                </div>
+            </form>
         </div>;
     }
 }
--- src/client/scripts/ui/views/RoomView.tsx
+++ src/client/scripts/ui/views/RoomView.tsx
@@ -1,6 +1,17 @@
 import * as React from "react";
 import {RouteComponentProps} from "react-router-dom";
-import {Room} from "../components/Room";
+import {Connection} from "../../Connection";
+import {GameManager} from "../../game/GameManager";
+import {OtherPlayer} from "../../game/OtherPlayer";
+import {Game} from "../components/room/Game";
+import {GameAlerts} from "../components/room/GameAlerts";
+import {GameControl} from "../components/room/GameControl";
+import {PlayerList} from "../components/room/PlayerList";
+import {RoomInfo} from "../components/room/RoomInfo";
+import {TurnInfo} from "../components/room/TurnInfo";
+import {WinnerInfo} from "../components/room/WinnerInfo";
+import {IRoomUI} from "../interfaces/IRoomUI";
+import {Routes} from "../Routes";
 
 export interface IRoomViewProps extends RouteComponentProps<IRoomViewProps> {
     roomid: string;
@@ -8,17 +19,99 @@ export interface IRoomViewProps extends RouteComponentProps<IRoomViewProps> {
 }
 
 export interface IRoomViewState {
-
+    players: OtherPlayer[];
+    activePlayer: IPlayer;
+    gameInfo: string;
+    winner: IPlayer;
+    gameManager: GameManager;
 }
 
-export class RoomView extends React.Component<IRoomViewProps, IRoomViewState> {
+export class RoomView extends React.Component<IRoomViewProps, IRoomViewState> implements IRoomUI {
+
+    constructor(props: IRoomViewProps) {
+        super(props);
+
+        this.leaveRoom = this.leaveRoom.bind(this);
+        this.startGame = this.startGame.bind(this);
+        this.updatePlayerlist = this.updatePlayerlist.bind(this);
+        this.updateGameInfo = this.updateGameInfo.bind(this);
+        this.updateWinner = this.updateWinner.bind(this);
+
+        this.state = {
+            players: [],
+            activePlayer: null,
+            gameInfo: "",
+            winner: null,
+            gameManager: null
+        };
+
+        Connection.setRoomname(this.props.roomid);
+    }
+
+    public getUsername() {
+        return this.props.username;
+    }
+
+    public getRoomID() {
+        return this.props.roomid;
+    }
+
+    public leaveRoom() {
+        if (this.state.gameManager !== null)
+            this.state.gameManager.actionLeaveRoom();
+    }
+
+    public updatePlayerlist(players: OtherPlayer[]) {
+        this.setState({players});
+    }
+
+    public updateTurnInfo(activePlayer: IPlayer): void {
+        this.setState({activePlayer});
+    }
+
+    public updateGameInfo(gameInfo: string): void {
+        this.setState({gameInfo});
+    }
+
+    public updateWinner(winner: IPlayer): void {
+        this.setState({winner});
+    }
+
+    public updateMission(mission: IMission): void {
+    }
+
+    public startGame() {
+        if (this.state.gameManager !== null)
+            this.state.gameManager.actionStartGame();
+    }
+
+    public leftRoom() {
+        window.location.href = Routes.linkToLobbyHREF();
+    }
+
+    public roomMessage(player: IPlayer, message: string): void {
+        console.log(player + ": " + message);
+        // ToDo Update Chat Window
+    }
+
+    public componentDidMount(): void {
+        Connection.getSocket((socket: SocketIOClient.Socket) => {
+            console.log("New Manager!");
+            this.setState({gameManager: new GameManager(socket, this)});
+        });
+    }
 
     public render() {
-        const roomid = this.props.match.params.roomid;
-        const username = this.props.match.params.username;
-
-        return <div>
-            <Room roomid={roomid} username={username}/>
+        return <div className={"content"}>
+            <Game/>
+            <GameControl actionStartGame={this.startGame} actionLeaveRoom={this.leaveRoom}/>
+            <div className={"info"}>
+                <RoomInfo roomid={this.props.roomid}/>
+                <WinnerInfo winner={this.state.winner}/>
+                <PlayerList players={this.state.players}/>
+                <TurnInfo player={this.state.activePlayer}/>
+                <GameAlerts alert={this.state.gameInfo}/>
+            </div>
         </div>;
     }
 }
