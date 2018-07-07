85a70a40ae22165bd1be642e61e3c02cc7c61566
Tim Beier <tim.beier@haw-hamburg.de>
2018-07-07 12:34:37 +0200

    Adjusted LobbyUi interface

--- src/client/scripts/ui/interfaces/ILobbyUI.ts
+++ src/client/scripts/ui/interfaces/ILobbyUI.ts
@@ -1,6 +1,8 @@
+import {LocalPlayer} from "../../game/LocalPlayer";
 
 export interface ILobbyUI {
     updateRoomList(rooms: string[] ): void;
+    joinedRoom(roomKey: string, roomName: string, localPlayer: LocalPlayer, otherPlayers: IPlayer[], gridInfo: IPlayer[][]): void;
     leftRoom(): void;
     otherLeftRoom(player: IPlayer): void;
     otherJoinedRoom(player: IPlayer): void;
