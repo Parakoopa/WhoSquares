ce8fedb9442f209ed21cfb0b9ccccefa22eb414b
Robin Müller <robin.mueller2@haw-hamburg.de>
2018-07-09 07:32:01 +0200

    grid fixed (ugly), player list with their colors

--- src/client/scripts/ui/components/room/PlayerList.tsx
+++ src/client/scripts/ui/components/room/PlayerList.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {OtherPlayer} from "../../../game/OtherPlayer";
+import {render} from "react-dom";
 
 export interface IPlayerListProps {
     players: IPlayer[];
@@ -10,14 +11,35 @@ export interface IPlayerListState {
 
 export class PlayerList extends React.Component<IPlayerListProps, IPlayerListState> {
 
+    private getActivePlayerColorHtml(player: IPlayer): string {
+        let color;
+        if (player == null)
+            color = 0;
+        else
+            color = player.color;
+
+        return "#" + color.toString(16);
+    }
+
+    private styleInfo(player: IPlayer): any {
+        const styleTurnInfo = {
+            color: this.getActivePlayerColorHtml(player),
+            fontWeight: 900,
+        };
+        return styleTurnInfo;
+    }
+
     public render(): any {
+
         const playerlist = this.props.players.map((player, i) =>
-            <li key={i}>{player.name}</li>
+            <li key={i} style={this.styleInfo(player)}>{player.name}</li>
         );
 
         return <div>
             <label>Players: </label>
-            {playerlist}
+            <ol className={"players"}>
+                {playerlist}
+            </ol>
         </div>;
     }
 }
